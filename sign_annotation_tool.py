161d0deced629904457c5eef38dd1b41c38127a3
KaiL4eK <darklexi494@gmail.com>
2017-07-16 01:15:55 +0300

    Added label addition

--- sign_annotation_tool.py
+++ sign_annotation_tool.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import os
+from time import sleep
 
 from Tkinter import *
 from PIL import Image
@@ -10,7 +11,7 @@ import cv2
 
 import json
 
-import Tkconstants, tkFileDialog
+import Tkconstants, tkFileDialog, tkSimpleDialog
 
 import argparse
 
@@ -35,6 +36,16 @@ current_frame = None
 image_widget = None
 
 ##############################################################
+
+def appendNewLabel(label_name):
+	global label_2_frame
+
+	newIntVar = IntVar()
+	Checkbutton(ChkBoxControlFrame, text=label_name, variable=newIntVar, command=labelCheckbox_cb).grid(row=len(label_list), column=0, sticky='w', pady=5)
+
+ 	label_list.append(label_name)
+ 	label_variables.append(newIntVar)
+ 	label_2_frame = np.hstack((label_2_frame, np.zeros((label_2_frame.shape[0],1), dtype=np.uint8)))
 
 def loadAnnotation():
 	global label_list, label_2_frame, label_variables
@@ -62,20 +73,18 @@ def saveAnnotation():
 	print('Dump successfully writed')
 
 def space_cb(event): saveAnnotation()
-
 root.bind("<space>", space_cb)
 
 def writeLabelValues():
-
-	for i, label in enumerate(label_list):
-		label_2_frame[current_frame_idx][i] = label_variables[i].get()
-
-		# print( label, 'is', label_variables[i].get() )
+	# print('Writing labels to cache')
+	for i, label_variable in enumerate(label_variables):
+		label_2_frame[current_frame_idx][i] = label_variable.get()
 
 def readLabelValues():
-
-	for i, label in enumerate(label_list):
-		label_variables[i].set( str(label_2_frame[current_frame_idx][i]) )
+	# print('Reading labels from cache')
+	for i, label_variable in enumerate(label_variables):
+		label_variable.set( str(label_2_frame[current_frame_idx][i]) )
+		# print(label_variable.get())
 
 def refresh_image():
 	# grab a reference to the image panels
@@ -127,7 +136,22 @@ videoControlFrame.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
 Button(videoControlFrame, text='Next ->', command=nextFrame_cb).pack(side=RIGHT, padx=5, pady=5)
 Button(videoControlFrame, text='<- Prev', command=prevFrame_cb).pack(side=LEFT, padx=5, pady=5)
 
-def leftKey(event): prevFrame_cb()
+command_executed = True
+
+def leftKey(event): 
+	global command_executed
+
+	def execute_command():
+		global command_executed
+		command_executed = True
+		prevFrame_cb()
+
+	if command_executed:
+		command_executed = False
+		root.after(50, execute_command)	
+
+
+
 def rightKey(event): nextFrame_cb()
 root.bind('<Left>', leftKey)
 root.bind('<Right>', rightKey)
@@ -138,6 +162,9 @@ if args.debug:
 	filepath = '/home/alexey/data/keras_traffic_NN/raw_data/CarRegister_Videos/EMER0007.MP4'
 else:
 	filepath = tkFileDialog.askopenfilename(initialdir = os.path.expanduser('~'), title = "Select file", filetypes = (("video files","*.mp4"),("all files","*.*")))
+
+if not filepath:
+	exit(1)
 
 dump_filepath = filepath.split('.')[0] + ".json"
 
@@ -153,12 +180,22 @@ controlFrame.grid(row=0, column=1, rowspan=2)
 ChkBoxControlFrame = Frame(controlFrame)
 ChkBoxControlFrame.grid(row=0, column=0)
 
-
 def labelCheckbox_cb():
 	writeLabelValues()
 
 for i, label in enumerate(label_list): 
 	Checkbutton(ChkBoxControlFrame, text=label, variable=label_variables[i], command=labelCheckbox_cb).grid(row=i, column=0, sticky='w', pady=5)
+
+ContolBtnsFrame = Frame(controlFrame)
+ContolBtnsFrame.grid(row=1, column=0)
+
+def addLabel_cb():
+	new_label_name = tkSimpleDialog.askstring("Label name prompt", "enter label name")
+	if new_label_name:
+		appendNewLabel(new_label_name)
+		print('Label added: ' + new_label_name)
+
+Button(ContolBtnsFrame, text='Add label', command=addLabel_cb).pack(side=TOP, padx=5, pady=5)
 
 ##############################################################
 
@@ -175,7 +212,7 @@ def read_frame(cap):
 	return read_frame
 
 cap = cv2.VideoCapture(filepath)
-frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT);
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT));
 
 if label_2_frame is None or len(label_list) != label_2_frame.shape[1]:
 	print('Initializing label array from scratch')
@@ -194,7 +231,7 @@ if args.sliding_mode:
 		readLabelValues()
 		refresh_image()
 
-	video_slider = Scale(videoControlFrame, from_=0, to=frame_count, orient=HORIZONTAL, command=videoTrackbarControl_cb, variable=slider_current_frame_idx)
+	video_slider = Scale(videoControlFrame, from_=0, to=frame_count-1, orient=HORIZONTAL, command=videoTrackbarControl_cb, variable=slider_current_frame_idx)
 	video_slider.pack(side=RIGHT, fill=BOTH, expand=True)
 
 root.resizable(0,0)
